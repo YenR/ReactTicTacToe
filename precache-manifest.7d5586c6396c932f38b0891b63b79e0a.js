1ea4006cc397f11ae2097582a1869c3963064a55
YenR <yen.0001@gmail.com>
2019-06-09 19:42:38 +0900

    Updates

---
+++ precache-manifest.7d5586c6396c932f38b0891b63b79e0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19af4f6f15537bf70bddd91b7245ba6",
+    "url": "/ReactTicTacToe/index.html"
+  },
+  {
+    "revision": "c4deb017380367cc797a",
+    "url": "/ReactTicTacToe/static/css/main.aabf46d8.chunk.css"
+  },
+  {
+    "revision": "01dd6f94fef530f524be",
+    "url": "/ReactTicTacToe/static/js/2.4bee3c64.chunk.js"
+  },
+  {
+    "revision": "c4deb017380367cc797a",
+    "url": "/ReactTicTacToe/static/js/main.bf06d0e8.chunk.js"
+  },
+  {
+    "revision": "e3c4d584acea7edad8a2",
+    "url": "/ReactTicTacToe/static/js/runtime~main.2a714852.js"
+  }
+]);
